906e4500d4e7cc9f9f731761538fd6e1babf4c38
eliasgz97 <eliasg229@gmail.com>
2019-05-10 16:06:18 -0600

    validaciones completadas

--- Lab4_EliasGiron.cpp
+++ Lab4_EliasGiron.cpp
@@ -4,6 +4,7 @@ using std::cout;
 using std::cin;
 using std::endl;
 using std::string;
+using std::stringstream;
 
 void imprimirmatriz(char** matrizmasmorra, int sizefilas, int columnas){
 	for(int i = 0; i < sizefilas; i++){
@@ -25,21 +26,40 @@ int main(){
 		cin >> caracteres;
 		columnas = caracteres.length();
 		char** matrizmasmorra = NULL;
-		matrizmasmorra = new char*[columnas];
+		matrizmasmorra = new char*[sizefilas];
 		for(int i = 0; i < columnas; i++){
-			matrizmasmorra[i] = new char[sizefilas];
+			matrizmasmorra[i] = new char[columnas];
 		}
 		for(int i = 0; i < columnas; i++){
 			matrizmasmorra[0][i] = caracteres[i];
 		}
 		for(int i = 1; i < sizefilas; i++){
-			for(int j = 0; j < columnas; j++){
-				if(matrizmasmorra[i-1][j] == '.' && matrizmasmorra[i-1][j+1] == '.' && matrizmasmorra[i-1][j+2] == '^'){
-					matrizmasmorra[i+1][j] = '.';
-				} /*else if (matrizmasmorra[i][j] == ) {
-
-				}
-				*/		
+			for(int j = 0; j < columnas; j++){	
+				if(j == 0){
+					if(matrizmasmorra[i-1][j] == '.' && matrizmasmorra[i-1][j+1] == '.'){
+						matrizmasmorra[i][j] = '.';
+					} else {
+						matrizmasmorra[i][j] = '^';
+					}
+				} else if(j == columnas-1){
+					if(matrizmasmorra[i-1][j-1] == '.' && matrizmasmorra[i-1][j] == '.'){
+						matrizmasmorra[i][j] = '.';
+					} else {
+						matrizmasmorra[i][j] = '^';
+					}
+				} else {
+					if(matrizmasmorra[i-1][j-1] == '.' && matrizmasmorra[i-1][j] == '.' && matrizmasmorra[i-1][j+1] == '^'){
+                                        	matrizmasmorra[i][j] = '^';
+					} else if(matrizmasmorra[i-1][j] == '^' && matrizmasmorra[i-1][j+1] == '.' && matrizmasmorra[i-1][j+1] == '.'){
+						matrizmasmorra[i][j] = '^';
+					} else if(matrizmasmorra[i][j-1] == '.' && matrizmasmorra[i-1][j] == '^' && matrizmasmorra[i-1][j+1] == '^'){
+						matrizmasmorra[i][j] = '^';
+					} else if(matrizmasmorra[i-1][j-1] == '^' && matrizmasmorra[i-1][j] == '^' && matrizmasmorra[i-1][j+1] == '.'){
+						matrizmasmorra[i][j] = '^';
+					} else {
+						matrizmasmorra[i][j] = '.';
+					}
+                                } 	
 			}
 		}
 		imprimirmatriz(matrizmasmorra, sizefilas, columnas);
