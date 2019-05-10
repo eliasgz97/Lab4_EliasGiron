a95180d37ec0085c0b0aaabf9cf1f83b3d8edc09
eliasgz97 <eliasg229@gmail.com>
2019-05-10 14:06:11 -0600

    For de la cadena de caracteres y de la matriz, comenzado

--- Lab4_EliasGiron.cpp
+++ Lab4_EliasGiron.cpp
@@ -7,12 +7,20 @@ using std::string;
 
 int main(){
 	char resp = 's';
-	int fila_inicial = 0, sizefilas = 0;
+	string caracteres = "";
+	int fila_inicial = 0, sizefilas = 0, columnas = 0;
 	while (resp == 's' || resp == 'S'){
 		cout <<"Ingrese la fila inicial de la matriz: "<<endl;
 		cin >> fila_inicial;
 		cout <<"Ingrese el numero de filas: "<<endl;
 		cin >> sizefilas;
+		cout << "Ingrese cadena de caracteres: "<<endl;
+		cin >> caracteres;
+		char** matrizmasmorra = NULL;
+		for(int i = 1; i <= caracteres.length(); i++){
+			columnas++;
+		}
+		cout << columnas;	
 		cout <<"Desea volver[s/n]: "<<endl;
 		cin >> resp;
 	}
